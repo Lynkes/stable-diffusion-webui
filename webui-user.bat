4a09cb0f1af6a4f90931c07356b4480154c50670
Pedro Guilherme Gabriel Maurer <48015282+Lynkes@users.noreply.github.com>
2023-05-29 20:15:01 -0300

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
+@title Stable Diffusion WebUI
 @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --xformers --medvram --no-half-vae --share --listen
 
 call webui.bat
